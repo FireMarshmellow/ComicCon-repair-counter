0c45ad4a3ccb3909744601a130db228c6816364b
Tomasz Burzy <62353091+FireMarshmellow@users.noreply.github.com>
2023-08-19 20:25:47 +0100

    set rtc time

--- set_rtc_time.py
+++ set_rtc_time.py
@@ -1,15 +1,15 @@
-import time
 from machine import I2C, Pin
 from ds1307 import DS1307
-from time import sleep
+
 # Initialize I2C
 i2c = I2C(0, scl=Pin(1), sda=Pin(0))  # Using GP1 for SCL and GP0 for SDA
 
 # Initialize DS1307 RTC
 rtc = DS1307(i2c)
 
-while True:
-    # Get the current time from the DS1307 RTC
-    print(rtc.datetime())
-    sleep(2)
+# Set the time: (year, month, day, weekday, hour, minute, second, subsecond)
+# For example, to set the time to August 12, 2023, 15:30:00 (3:30 PM):
+rtc.datetime((2023, 8, 19, 6, 20, 25, 0, 0))
 
+print("Time set successfully!")
+print(rtc.datetime())
