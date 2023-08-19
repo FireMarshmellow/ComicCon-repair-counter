1fd0c7f820530084fc975c5e11ea5c9e9b7987bf
Tomasz Burzy <62353091+FireMarshmellow@users.noreply.github.com>
2023-08-19 14:32:52 +0100

    added undo

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import machine
 from machine import I2C, Pin
 import ssd1306
-import utime
+from time import sleep, sleep_ms
 from ds1307 import DS1307
 from large_font import font as large_font
 import math
 import urandom
 import os
+import utime
+
 
 # Constants
 OLED_WIDTH = 128
@@ -17,86 +19,94 @@ MAX_RADIUS = int((OLED_WIDTH**2 + OLED_HEIGHT**2)**0.5)
 LOG_FILE = 'button_presses.csv'
 INACTIVITY_THRESHOLD = 10  # in seconds
 
+# Initialize OLED I2C
+i2c_oled = machine.SoftI2C(scl=Pin(15), sda=Pin(14))
+oled = ssd1306.SSD1306_I2C(OLED_WIDTH, OLED_HEIGHT, i2c_oled)
 
-class OLEDManager:
-    def __init__(self):
-        self.i2c_oled = machine.SoftI2C(scl=Pin(15), sda=Pin(14))
-        self.oled = ssd1306.SSD1306_I2C(OLED_WIDTH, OLED_HEIGHT, self.i2c_oled)
+# Initialize the button
+button = Pin(16, Pin.IN, Pin.PULL_UP)
+button2 = Pin(20, Pin.IN, Pin.PULL_UP)
 
-    def display_partitioned_screen(self, num1, num2, time_str):
-        self.oled.fill(0)
-        for y in range(OLED_HEIGHT):
-            self.oled.pixel(OLED_WIDTH // 2, y, 1)
-        for x in range(OLED_WIDTH // 2, OLED_WIDTH):
-            self.oled.pixel(x, OLED_HEIGHT // 2, 1)
-        self.draw_big_text(str(num1), 0, 8)
-        self.oled.text(str(num2), 70, 2)
-        self.oled.text(time_str, 70, 18)
-        self.oled.show()
+# Initialize DS1307 RTC I2C
+i2c_rtc = I2C(0, scl=Pin(1), sda=Pin(0))
+rtc = DS1307(i2c_rtc)
 
-    def draw_big_text(self, text, x, y):
-        for char in text:
-            if char in large_font:
-                self.draw_large_char(char, x, y)
-                x += 18
+# Get the current datetime for initialization
+year, month, day, _, hour, _, _, _ = rtc.datetime()
 
-    def draw_large_char(self, char, x, y):
-        for row in range(16):
-            for col in range(16):
-                pixel = (large_font[char][row] >> (15 - col)) & 1
-                self.oled.pixel(x + col, y + row, pixel)
+# Check if the log file exists, if not, create it
+if LOG_FILE not in os.listdir():
+    with open(LOG_FILE, 'w') as f:
+        f.write('Date,Hour,Count\n')
 
+# ... (rest of the function definitions)
 
-class ButtonLogger:
-    def __init__(self):
-        if LOG_FILE not in os.listdir():
-            with open(LOG_FILE, 'w') as f:
-                f.write('Date,Hour,Count\n')
+def log_button_press(date, hour):
+    with open(LOG_FILE, 'r') as f:
+        lines = f.readlines()
 
-    def log_button_press(self, date, hour):
+    for idx, line in enumerate(lines):
+        if line.startswith("{:04}-{:02}-{:02},{:02}".format(date[0], date[1], date[2], hour)):
+            current_count = int(line.strip().split(",")[2])
+            lines[idx] = "{:04}-{:02}-{:02},{:02},{:d}\n".format(date[0], date[1], date[2], hour, current_count + 1)
+            break
+    else:
+        lines.append("{:04}-{:02}-{:02},{:02},1\n".format(date[0], date[1], date[2], hour))
+
+    with open(LOG_FILE, 'w') as f:
+        for line in lines:
+            f.write(line)
+
+def get_today_button_presses_for_hour(year, month, day, hour):
+    count = 0
+    try:
         with open(LOG_FILE, 'r') as f:
             lines = f.readlines()
-
-        for idx, line in enumerate(lines):
-            if line.startswith("{:04}-{:02}-{:02},{:02}".format(date[0], date[1], date[2], hour)):
-                current_count = int(line.strip().split(",")[2])
-                lines[idx] = "{:04}-{:02}-{:02},{:02},{:d}\n".format(date[0], date[1], date[2], hour, current_count + 1)
-                break
-        else:
-            lines.append("{:04}-{:02}-{:02},{:02},1\n".format(date[0], date[1], date[2], hour))
-
-        with open(LOG_FILE, 'w') as f:
             for line in lines:
-                f.write(line)
+                if line.startswith("{:04}-{:02}-{:02},{:02}".format(year, month, day, hour)):
+                    count = int(line.strip().split(",")[2])
+                    break
+    except:
+        pass
+    return count
 
-    def get_today_button_presses_for_hour(self, year, month, day, hour):
-        count = 0
-        try:
-            with open(LOG_FILE, 'r') as f:
-                lines = f.readlines()
-                for line in lines:
-                    if line.startswith("{:04}-{:02}-{:02},{:02}".format(year, month, day, hour)):
-                        count = int(line.strip().split(",")[2])
-                        break
-        except:
-            pass
-        return count
+def draw_large_char(oled, char, x, y):
+    for row in range(16):
+        for col in range(16):
+            pixel = (large_font[char]	[row] >> (15 - col)) & 1
+            oled.pixel(x + col, y + row, pixel)
 
-    def get_today_total_presses(self, year, month, day):
-        total_presses = 0
-        try:
-            with open(LOG_FILE, 'r') as f:
-                lines = f.readlines()
-                for line in lines:
-                    if line.startswith("{:04}-{:02}-{:02}".format(year, month, day)):
-                        count = int(line.strip().split(",")[2])
-                        total_presses += count
-        except:
-            pass
-        return total_presses
+def draw_big_text(oled, text, x, y):
+    for char in text:
+        if char in large_font:
+            draw_large_char(oled, char, x, y)
+            x += 18
 
+def display_partitioned_screen(num1, num2, time_str):
+    oled.fill(0)
+    for y in range(OLED_HEIGHT):
+        oled.pixel(OLED_WIDTH // 2, y, 1)
+    for x in range(OLED_WIDTH // 2, OLED_WIDTH):
+        oled.pixel(x, OLED_HEIGHT // 2, 1)
+    draw_big_text(oled, str(num1), 0, 8)
+    oled.text(str(num2), 70, 2)
+    oled.text(time_str, 70, 18)
+    oled.show()
 
-def firework_animation(oled):
+def get_today_total_presses(year, month, day):
+    total_presses = 0
+    try:
+        with open(LOG_FILE, 'r') as f:
+            lines = f.readlines()
+            for line in lines:
+                if line.startswith("{:04}-{:02}-{:02}".format(year, month, day)):
+                    count = int(line.strip().split(",")[2])
+                    total_presses += count
+    except:
+        pass
+    return total_presses
+
+def firework_animation():
     def draw_firework(radius):
         for angle in range(0, 360, 5):
             random_angle = angle + urandom.randint(-10, 10)
@@ -117,73 +127,58 @@ def firework_animation(oled):
         if radius > 20:
             oled.text(selected_phrase, (OLED_WIDTH - len(selected_phrase) * 8) // 2, OLED_HEIGHT // 2)
         oled.show()
-        utime.sleep_ms(FIREWORK_DELAY)
-    utime.sleep(0.1)
+        sleep_ms(FIREWORK_DELAY)
+    sleep(0.1)
 
+# Track the last time of activity
+last_activity = utime.time()
 
-def main():
-    oled_manager = OLEDManager()
-    button_logger = ButtonLogger()
+counter1 = get_today_total_presses(year, month, day)
+last_hour = None
+button_presses_this_hour = get_today_button_presses_for_hour(year, month, day, hour)
 
-    button_increase = Pin(16, Pin.IN, Pin.PULL_UP)
-    button_decrease = Pin(20, Pin.IN, Pin.PULL_UP)
+while True:
+    current_time = utime.time()  # <-- Using utime.time()
+    inactivity_duration = current_time - last_activity
 
-    i2c_rtc = I2C(0, scl=Pin(1), sda=Pin(0))
-    rtc = DS1307(i2c_rtc)
+    if inactivity_duration > INACTIVITY_THRESHOLD:
+        oled.poweroff()  # Turn off the OLED display
+    else:
+        oled.poweron()  # Ensure the OLED display is on
 
-    year, month, day, _, hour, _, _, _ = rtc.datetime()
+    year, month, day, _, hour, minute, second, _ = rtc.datetime()
+    time_str = "{:02}:{:02}:{:02}".format(hour, minute, second)
 
-    last_activity = utime.time()
-    counter1 = button_logger.get_today_total_presses(year, month, day)
-    last_hour = None
-    button_presses_this_hour = button_logger.get_today_button_presses_for_hour(year, month, day, hour)
+    if not button.value():
+        last_activity = utime.time()  # <-- Update the last_activity immediately
+        oled.poweron()  # Make sure OLED is powered on for visual feedback
+        firework_animation()
+        counter1 += 1
+        log_button_press((year, month, day), hour)
 
-    while True:
-        current_time = utime.time()
-        inactivity_duration = current_time - last_activity
-
-        if inactivity_duration > INACTIVITY_THRESHOLD:
-            oled_manager.oled.poweroff()
+        if last_hour is None or last_hour != hour:
+            last_hour = hour
+            button_presses_this_hour = get_today_button_presses_for_hour(year, month, day, hour)
         else:
-            oled_manager.oled.poweron()
+            button_presses_this_hour += 1
 
-        year, month, day, _, hour, minute, second, _ = rtc.datetime()
-        time_str = "{:02}:{:02}:{:02}".format(hour, minute, second)
+        sleep(BUTTON_DEBOUNCE_DELAY)
 
-        if not button_increase.value():
-            last_activity = utime.time()
-            oled_manager.oled.poweron()
-            firework_animation(oled_manager.oled)
-            counter1 += 1
-            button_logger.log_button_press((year, month, day), hour)
+    if not button2.value():
+        last_activity = utime.time()  # <-- Update the last_activity immediately
+        oled.poweron()  # Make sure OLED is powered on for visual feedback
+        firework_animation()
+        counter1 -= 1  # Decrease the counter
+        log_button_press((year, month, day), hour)  # Log the button press
 
-            if last_hour is None or last_hour != hour:
-                last_hour = hour
-                button_presses_this_hour = button_logger.get_today_button_presses_for_hour(year, month, day, hour)
-            else:
-                button_presses_this_hour += 1
+        if last_hour is None or last_hour != hour:
+            last_hour = hour
+            button_presses_this_hour = get_today_button_presses_for_hour(year, month, day, hour)
+        else:
+            button_presses_this_hour -= 1  # Decrease the hourly count
 
-            utime.sleep(BUTTON_DEBOUNCE_DELAY)
+        sleep(BUTTON_DEBOUNCE_DELAY)
 
-        if not button_decrease.value():
-            last_activity = utime.time()
-            oled_manager.oled.poweron()
-            firework_animation(oled_manager.oled)
-            counter1 -= 1
-            button_logger.log_button_press((year, month, day), hour)
-
-            if last_hour is None or last_hour != hour:
-                last_hour = hour
-                button_presses_this_hour = button_logger.get_today_button_presses_for_hour(year, month, day, hour)
-            else:
-                button_presses_this_hour -= 1
-
-            utime.sleep(BUTTON_DEBOUNCE_DELAY)
-
-        oled_manager.display_partitioned_screen(counter1, button_presses_this_hour, time_str)
-        counter1 %= 1000
-        utime.sleep(0.1)
-
-
-if __name__ == "__main__":
-    main()
+    display_partitioned_screen(counter1, button_presses_this_hour, time_str)
+    counter1 %= 1000
+    sleep(0.1)
